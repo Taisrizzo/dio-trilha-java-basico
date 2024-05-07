e536170fc46b3370f7d738606cee1a08c6175e11
Tais Rizzo <tais_margo@hotmail.com>
2024-05-07 18:42:35 -0300

    Aprendendo sobre os tipos de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+      
+   String nomeUm = "Tais";
+   String nomeDois = new String("Tais");
+
+   System.out.println(nomeUm.equals(nomeDois));
+        
+    }
+    
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        //estudar classe String que representa texto nas linguagens.
+        
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+       final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
+
     }
-}
+} 
