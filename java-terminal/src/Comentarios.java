362ce13e9fe3a83ad2811fef88a7bc83cd98536e
Tais Rizzo <tais_margo@hotmail.com>
2024-05-15 18:36:36 -0300

    commit com comentarios

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,9 @@
+public class Comentarios {
+    public static void main(String[] args) {
+
+
+    //Este código a cima é um exemplo do que NÃO FAZER! O código está totalemnte bagunçado e não é claro para quem lê, e provavelmente nem executa.//
+
+    
+    }
+}
