1a5d0f19a597f4230223371b4ab3da0ecadd5d0c
Tais Rizzo <tais_margo@hotmail.com>
2024-05-20 19:44:36 -0300

    commit do meu projeto conta banco finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,9 +2,12 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-        public static void main(String[] args) {
-            
+        public static void main(String[] args) throws Exception {
+
             Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Por favor, digite o nome do cliente!");
+            String nomeCliente = scanner.nextLine();
 
             System.out.println("Por favor, digite o número da conta!");
             int numero = scanner.nextInt();
@@ -12,12 +15,10 @@ public class ContaTerminal {
             System.out.println("Por favor, digite o número da agência!");
             int agencia = scanner.nextInt();
 
-            System.out.println("Por favor, digite o nome do cliente!");
-            String nomeCliente = scanner.next();
-
             System.out.println("Por favor, digite o saldo!");
             double saldo = scanner.nextDouble();
 
-            System.out.println("Olá, "+nomeCliente+" obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.");
+
+            System.out.println("Olá, "+nomeCliente +" obrigado por criar uma conta em nosso banco. Sua agência é "+agencia+", sua conta "+numero+", e seu saldo de R$"+saldo+" já está disponível para saque.");
         }
 }
